c841cd4118524283e2ab9692d7044b30c21543bd
Krishna Bhunia <bhunia.k@pg.com>
2024-11-05 11:52:30 +0530

    cos2

--- test_web_socket.py
+++ test_web_socket.py
@@ -3,12 +3,15 @@
 import asyncio
 from websockets.server import serve
 
+
 async def echo(websocket):
     async for message in websocket:
         await websocket.send(message)
 
+
 async def main():
     async with serve(echo, "localhost", 8765):
         await asyncio.get_running_loop().create_future()  # run forever
+
 
 asyncio.run(main())
