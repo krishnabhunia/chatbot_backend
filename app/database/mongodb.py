daca158cf52b2a6d6f0a5606ca4074faf8e97e30
Krishna Bhunia <bhunia.k@pg.com>
2024-11-07 11:28:26 +0530

    test code

--- app/database/mongodb.py
+++ app/database/mongodb.py
@@ -1,6 +1,6 @@
 from pymongo import MongoClient
 
 # MongoDB connection setup
-client = MongoClient("mongodb://localhost:27017/")
+client: MongoClient = MongoClient("mongodb://<WINDOWS_IP>:27017")
 db = client.chatbot_db
 collection = db.conversations
